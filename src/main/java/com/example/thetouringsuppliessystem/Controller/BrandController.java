def214018a60895fa00dbf27113076971373fb30
Abdulrahman Bamrie <115091048+abdabd12320@users.noreply.github.com>
2024-08-19 07:51:36 +0300

    Add files via upload

---
+++ src/main/java/com/example/thetouringsuppliessystem/Controller/BrandController.java
@@ -0,0 +1,52 @@
+package com.example.thetouringsuppliessystem.Controller;
+
+import com.example.thetouringsuppliessystem.Api.ApiResponse;
+import com.example.thetouringsuppliessystem.Model.Brand;
+import com.example.thetouringsuppliessystem.Service.BrandService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/brand")
+public class BrandController {
+
+    private final BrandService brandService;
+
+    @GetMapping("/get")
+    public ResponseEntity getBrands()
+    {
+        return ResponseEntity.status(200).body(brandService.getBrands());
+    }
+    @PostMapping("/add")
+    public ResponseEntity addBrand(@Valid@RequestBody Brand brand, Errors errors)
+    {
+        if(errors.hasErrors())
+        {
+            String m = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(m);
+        }
+        brandService.addBrand(brand);
+        return ResponseEntity.status(200).body(new ApiResponse("Brand added"));
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateBrand(@PathVariable Integer id,@Valid@RequestBody Brand brand,Errors errors)
+    {
+        if(errors.hasErrors())
+        {
+            String m = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(m);
+        }
+        brandService.updateBrand(id, brand);
+        return ResponseEntity.status(200).body(new ApiResponse("Brand updated"));
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteBrand(@PathVariable Integer id)
+    {
+        brandService.deleteBrand(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Brand deleted"));
+    }
+}
---
+++ src/main/java/com/example/thetouringsuppliessystem/Controller/VendorController.java
@@ -0,0 +1,52 @@
+package com.example.thetouringsuppliessystem.Controller;
+
+import com.example.thetouringsuppliessystem.Api.ApiResponse;
+import com.example.thetouringsuppliessystem.Model.Vendor;
+import com.example.thetouringsuppliessystem.Service.VendorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/vendor")
+public class VendorController {
+
+    private final VendorService vendorService;
+
+    @GetMapping("/get")
+    public ResponseEntity getVendors()
+    {
+        return ResponseEntity.status(200).body(vendorService.getVendors());
+    }
+    @PostMapping("/add")
+    public ResponseEntity addVendor(@Valid@RequestBody Vendor vendor, Errors errors)
+    {
+        if(errors.hasErrors())
+        {
+            String m = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(m);
+        }
+        vendorService.addVendor(vendor);
+        return ResponseEntity.status(200).body(new ApiResponse("Vendor added"));
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateVendor(@PathVariable Integer id,@Valid@RequestBody Vendor vendor, Errors errors)
+    {
+        if(errors.hasErrors())
+        {
+            String m = errors.getFieldError().getDefaultMessage();
+            return ResponseEntity.status(400).body(m);
+        }
+        vendorService.updateVendor(id, vendor);
+        return ResponseEntity.status(200).body(new ApiResponse("Vendor updated"));
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteVendor(@PathVariable Integer id)
+    {
+        vendorService.deleteVendor(id);
+        return ResponseEntity.status(200).body(new ApiResponse("Vendor deleted"));
+    }
+}
